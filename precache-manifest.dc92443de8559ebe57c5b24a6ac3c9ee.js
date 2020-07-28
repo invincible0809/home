838f6d01a64affc0f6dfbe3eebe67d037cb8e7a2
invincible0809 <sughoshpdixit@gmail.com>
2020-07-28 15:19:36 +0530

    Updates

---
+++ precache-manifest.dc92443de8559ebe57c5b24a6ac3c9ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837fb8b281a629f47884c60b28a0e493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0897cc2b10c193cf587d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aa3a4f8de3a57a437dcf",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "0897cc2b10c193cf587d",
+    "url": "/static/js/2.bca434be.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/static/js/2.bca434be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3a4f8de3a57a437dcf",
+    "url": "/static/js/main.4f2d655c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "030b65d4c9e7777e13ee4fc3d9d4b7e0",
+    "url": "/static/media/resume.030b65d4.pdf"
+  }
+]);
